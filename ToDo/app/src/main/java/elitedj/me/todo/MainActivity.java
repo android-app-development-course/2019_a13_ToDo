1df44ba8c0fc9bb9d8a4a3e8f569576bcef8bb56
Elitedj <747527651@qq.com>
2019-11-21 11:04:44 +0800

    登录界面初稿

--- ToDo/app/src/main/java/elitedj/me/todo/MainActivity.java
+++ ToDo/app/src/main/java/elitedj/me/todo/MainActivity.java
@@ -1,7 +1,10 @@
 package elitedj.me.todo;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 系统透明状态栏
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
+            localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+        }
+
+
     }
 }
