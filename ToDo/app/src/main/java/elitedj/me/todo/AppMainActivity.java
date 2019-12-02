237e4fd7e710c76c998b1f990e5d37d5db3aed62
Elitedj <747527651@qq.com>
2019-12-03 00:33:21 +0800

    update discoverView UI

--- ToDo/app/src/main/java/elitedj/me/todo/AppMainActivity.java
+++ ToDo/app/src/main/java/elitedj/me/todo/AppMainActivity.java
@@ -2,39 +2,29 @@ package elitedj.me.todo;
 
 import android.app.LocalActivityManager;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Build;
 import android.support.annotation.NonNull;
-import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
 
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.github.siyamed.shapeimageview.RoundedImageView;
 import com.yinglan.alphatabs.AlphaTabsIndicator;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import elitedj.me.todo.Adapter.MomentListAdapter;
+import elitedj.me.todo.TodoList.DiscoverActivity;
 import elitedj.me.todo.TodoList.ListActivity;
 
 
@@ -42,14 +32,12 @@ public class AppMainActivity extends AppCompatActivity {
 
     private ImageView touxiang;
     private ListView lv1;
-    private RecyclerView moment_list;
     private AlphaTabsIndicator bottomTab;
     private ViewPager viewPager;
     private View listView, dataView, discoverView, meView;
     private List<View> views = new ArrayList<>();
-    private ArrayList<Moment> moments = new ArrayList<>();
     private LocalActivityManager manager;
-    private Intent intentList;
+    private Intent intentList, intentDiscover;
     private int[] imagesId={R.drawable.finish,R.drawable.target,R.drawable.core,R.drawable.fengjing,R.drawable.color,R.drawable.set,R.drawable.help,R.drawable.share};
     private	String[] names={"历史记录时间轴","未来时间表","ToDo核心设置","背景海报图设置","主题颜色","更多外观 | 其他设置","帮助","分享给朋友"};
     private  String[] contents={"已完成计划的记录","重要日期倒计时","铃声震动|休息时长","计时或锁机时的背景海报","自定义主题颜色","卡片背景|主界面背景|语言设置","常见的使用问题和解决方法","如果你觉得好用就分享给你的小伙伴们呗"};
@@ -73,12 +61,19 @@ public class AppMainActivity extends AppCompatActivity {
         bottomTab = findViewById(R.id.bottomTab);
         viewPager = findViewById(R.id.viewPage);
         LayoutInflater inflater = getLayoutInflater();
+
         // 关联ListActivity
         intentList = new Intent(AppMainActivity.this, ListActivity.class);
         listView = manager.startActivity("viewID", intentList).getDecorView();
         //listView = inflater.inflate(R.layout.activity_list, null);
+
         dataView = inflater.inflate(R.layout.activity_data, null);
-        discoverView = inflater.inflate(R.layout.activity_discover, null);
+
+        // 关联DiscoverActivity
+        intentDiscover = new Intent(AppMainActivity.this, DiscoverActivity.class);
+        discoverView = manager.startActivity("viewID", intentDiscover).getDecorView();
+        //discoverView = inflater.inflate(R.layout.activity_discover, null);
+
         meView = inflater.inflate(R.layout.activity_me, null);
         views.add(listView);
         views.add(dataView);
@@ -92,7 +87,6 @@ public class AppMainActivity extends AppCompatActivity {
         //lv1.setAdapter(mAdapter);
         lv1.setAdapter(mAdapter);
 
-        discoverViewInit();
 
         //头像按钮
         touxiang = (ImageView) meView.findViewById(R.id.touxiang);
@@ -105,45 +99,6 @@ public class AppMainActivity extends AppCompatActivity {
         });
     }
 
-
-
-
-    public void discoverViewInit(){
-        // 设置朋友圈的titleBar的margin，让它在系统状态栏底部，适配所有手机
-        LinearLayout titleBar = discoverView.findViewById(R.id.linearlayout1);
-        // LinearLayout 表示该控件在LinearLayout里面，其他控件用法类似
-        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(titleBar.getLayoutParams());
-        lp.setMargins(0,new NativeBarHeight().getNativeBarHeight(AppMainActivity.this)+16,0,0);
-        titleBar.setLayoutParams(lp);
-
-        AppBarLayout appBarLayout = discoverView.findViewById(R.id.appBar);
-        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
-            @Override
-            public void onOffsetChanged(AppBarLayout appBarLayout, int i) {
-                RoundedImageView face = discoverView.findViewById(R.id.myFace);
-                CoordinatorLayout bg = discoverView.findViewById(R.id.discoverViewBG);
-                if(Math.abs(i) >= appBarLayout.getTotalScrollRange()) { //折叠
-                    face.setVisibility(View.GONE);
-                } else { //未折叠
-                    bg.setBackgroundColor(getResources().getColor(R.color.white));
-                    face.setVisibility(View.VISIBLE);
-                }
-            }
-        });
-
-        // 朋友圈的列表
-        moment_list = discoverView.findViewById(R.id.momentlist);
-        for(int i=1;i<=15;i++){
-            Moment moment = new Moment();
-            moment.setFace(R.drawable.img3);
-            moment.setName((char)(i+'A')+"");
-            moment.setContent("ajsdfhajklsdghlasdkghasjkldghasjkgadfgdfh");
-            moments.add(moment);
-        }
-        moment_list.setLayoutManager(new LinearLayoutManager(discoverView.getContext()));
-        MomentListAdapter momentListAdapter = new MomentListAdapter(moments, getLayoutInflater());
-        moment_list.setAdapter(momentListAdapter);
-    }
 
     //viewPager的Adapter
     private class pagerAdapter extends PagerAdapter
